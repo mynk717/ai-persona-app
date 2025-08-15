60cdea3e7b442a81b60bacb60ddeae75ef3ce3e6
mynk717 <shukla.mayank247@gmail.com>
2025-08-15 15:06:43 +0530

    fix: Removed App.css import to fix build error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useRef, useEffect } from "react";
-import "./App.css";
 import { personaConfig, colors } from "./config";
 import PersonaSelector from "./components/PersonaSelector";
 import MessageBubble from "./components/MessageBubble";
